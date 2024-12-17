b79e665d70a3db187fcf4aa16fbecff47a2d5e83
petaboss2000 <petaboss2000@gmail.com>
2024-12-17 23:44:41 +0300

    Заменил appendChild на перебор массива со всеми сообщениями и добавления их в jsx разметке, в файле ChatList.jsx

--- src/pages/ChatListPageFolder/ChatList.jsx
+++ src/pages/ChatListPageFolder/ChatList.jsx
@@ -1,41 +1,41 @@
-import React, {useEffect, useRef} from "react";
+import React, { useEffect, useRef, useState } from "react";
 import Cookies from 'js-cookie';
 import Chat from "./Chat";
 
 const ChatList = () => {
-
-	const usersDivRef = useRef(null);
+	const [chats, setChats] = useState([]); // Состояние для списка чатов
 	const socket = useRef(null);
+	const usersDivRef = useRef(null);
 
 	useEffect(() => {
-		const cookieValue = Cookies.get('address')
+		const cookieValue = Cookies.get('address');
+
+		// Подключение к WebSocket
 		socket.current = new WebSocket(`ws://localhost:5000/chats/${cookieValue}`);
 
-		socket.current.onopen = function () {
+		socket.current.onopen = () => {
 			console.log("Соединение установлено!!");
 		};
 
-		socket.current.onmessage = function (event) {
-			console.log("Get message");
+		socket.current.onmessage = (event) => {
+			console.log("Получены данные");
 			console.log(event.data);
+
 			const data = JSON.parse(event.data);
-			usersDivRef.current.replaceChildren()
-			data.chats.forEach(chat => {
-				if (usersDivRef.current) {
-					const chatComponent = React.createElement(Chat, {chat_id: chat.chat_id}, null);
-					usersDivRef.current.appendChild(chatComponent);
-				}
-			});
+
+			// Обновляем состояние чатов
+			setChats(data.chats);
 		};
 
-		socket.current.onclose = function (event) {
+		socket.current.onclose = () => {
 			console.log("Соединение закрыто");
 		};
 
-		socket.current.onerror = function (error) {
+		socket.current.onerror = (error) => {
 			console.log(`Ошибка: ${error.message}`);
 		};
 
+		// Закрываем соединение при размонтировании компонента
 		return () => {
 			if (socket.current) {
 				socket.current.close();
@@ -45,6 +45,9 @@ const ChatList = () => {
 
 	return (
 		<div className="ChatList" ref={usersDivRef}>
+			{chats.map((chat) => (
+				<Chat key={chat.chat_id} chat_id={chat.chat_id} />
+			))}
 		</div>
 	);
 };
