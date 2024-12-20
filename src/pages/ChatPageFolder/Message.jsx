f9533a57832971084230ca87f3a1ab5d139200ff
petaboss2000 <petaboss2000@gmail.com>
2024-12-20 10:10:11 +0300

    Билд для подключения к серверу, исправлено не корректная установка класс для massage

--- src/pages/ChatPageFolder/Message.jsx
+++ src/pages/ChatPageFolder/Message.jsx
@@ -1,10 +1,9 @@
 import React from "react";
-import { Link } from 'react-router-dom';
 
 const Message = (props) => {
 
 	return (
-		<div className={"Message" + " " + props.className}>
+		<div className={`Message ${props.className}`}>
 			{props.messageText}
 		</div>
 	);
